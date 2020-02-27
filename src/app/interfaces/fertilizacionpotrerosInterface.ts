d161efc03db44c9e1a7d6b981508dca1352bed4f
Fabio Quintero <fabio.quintero@gmail.com>
2020-02-27 08:50:40 -0500

    Subimos Intrerface FumigacionPotreros y FertilizacionPotreros

---
+++ src/app/interfaces/fertilizacionpotrerosInterface.ts
@@ -0,0 +1,8 @@
+/* Modelo del estado FertilizacionPotrero*/
+export interface ModelFertilizacionPotrero {
+    id: string;
+    idPotrero: string;
+    fecha: string;
+    Observaciones: string;
+    idresponsable: string;
+ }
---
+++ src/app/interfaces/fumigacionpotrerosInterface.ts
@@ -0,0 +1,8 @@
+/* Modelo del estado FumigacionPotrero*/
+export interface ModelFumigacionPotrero {
+    id: string;
+    idPotrero: string;
+    fecha: string;
+    Observaciones: string;
+    idresponsable: string;
+ }
