b640d5cb3c4410a0d89242d9ab5b3c29dab356cf
Johnny Alexander Salazar <alexander9052@gmail.com>
2020-02-28 18:55:30 -0500

    Enviroment apuntando al hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost/MacapaAppBackend/'
+  baseUrl: 'http://hatolamacana.com/MacapaAppBackend/'
+  /* baseUrl: 'http://localhost/MacapaAppBackend/' */
 };
 
 /*
