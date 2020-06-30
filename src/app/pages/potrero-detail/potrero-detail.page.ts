a9c3e6fbb893d771c2a2c3903b6f66ae646c3451
Fabio Quintero <fabio.quintero@gmail.com>
2020-06-30 10:21:58 -0500

    Ajustes a potrero y aforo

--- src/app/pages/potrero-detail/potrero-detail.page.ts
+++ src/app/pages/potrero-detail/potrero-detail.page.ts
@@ -48,8 +48,9 @@ export class PotreroDetailPage implements OnInit {
         this.potreroData.observacion = this.router.getCurrentNavigation().extras.state.observacion;
         this.idRotacionTemp = this.router.getCurrentNavigation().extras.state.idrotacion;
         this.potreroData.idresponsable = this.router.getCurrentNavigation().extras.state.idresponsable;
-        this.potreroData.estado = this.helperService.procesarCheckboxRadioValue(this.router.getCurrentNavigation().extras.state.estado);
-        // console.log("El estado es "+ this.potreroData.estado);
+        this.potreroData.estado = this.router.getCurrentNavigation().extras.state.estado;
+        this.potreroData.ordenrotacion = this.router.getCurrentNavigation().extras.state.ordenrotacion
+        console.log("El orden  es "+ this.potreroData.ordenrotacion);
       }
     });    
   }
